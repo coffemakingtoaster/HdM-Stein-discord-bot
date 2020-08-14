9fc0cf7eb55a5af502b3821ecd8c3c5f58862307
coffemakingtoaster <mh340@hdm-stuttgart.de>
2020-08-14 20:28:50 +0200

    slight changes

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import os
 import asyncio
 import discord
 from dotenv import load_dotenv
-from discord.ext import commands 
+from discord.ext import commands
+import json 
 
 load_dotenv()
 
@@ -13,6 +14,13 @@ bot = commands.Bot(command_prefix = "!")
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+
+@bot.command(name="addevent",help="Use this to create events!")
+@commands.has_role("bot dev")
+async def on_message(ctx):
+    
+    
+
 
 @bot.command(name="test",help="This is a test!")
 @commands.has_role("bot dev")
