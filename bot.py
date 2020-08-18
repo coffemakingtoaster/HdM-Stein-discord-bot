b37b852fb70ef99c60a0e1395971bc6c4a378071
coffemakingtoaster <mh340@hdm-stuttgart.de>
2020-08-18 15:08:47 +0200

    Added automated cleanup every 2 minutes (might raise to 24h)

--- bot.py
+++ bot.py
@@ -2,11 +2,13 @@ import os
 import asyncio
 import discord
 from dotenv import load_dotenv
-from discord.ext import commands
+from discord.ext import commands,tasks
 import json 
 import discord_events
+from aiofiles.os import remove
 
 load_dotenv()
+
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 PATH = os.getenv('FILE_PATH')
@@ -15,30 +17,43 @@ bot = commands.Bot(command_prefix = "!")
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    bot.loop.create_task(cleanup())
 
 @bot.command(name="listevent", help ="Use this to list all events!")
 @commands.has_role("bot dev")
 async def on_message(ctx):
-    event_list = discord_events.events.list()
+    event_list = discord_events.events.list(os.getcwd())
     message = ""
-    for item in event_list:
+    for i in event_list:
+        item = event_list[i]
+        print(item)
         message+= item["event_name"]+"\t"+item["event_date"]+"\t"+str(len(item["event_participants"]))+"\n"
-    await channel.send(message)
+    await ctx.send(message)
         
 
 @bot.command(name="addevent",help="Use this to create events! This requires 2 parameters the Eventname (in quotation marks) and the event_date (DD/MM/YYYY:HH:MM). Use !addeventdesc")
 @commands.has_role("bot dev")
 async def on_message(ctx,event_name,event_date):
     if event_name and event_date:
-        uID = discord_events.events.generate_event(event_name,event_date)
-        await channel.send("Event was succcessfully generated and added to the db! Your EventID is ",uID)
+        uID = discord_events.events.generate_event(os.getcwd(),event_name,event_date)
+        await ctx.send("Event was succcessfully generated and added to the db! Your EventID is "+str(uID))
         
     
-
 
 @bot.command(name="test",help="This is a test!")
 @commands.has_role("bot dev")
 async def on_message(ctx):
     await ctx.send("success")
+
+
+async def cleanup():
+    print("scanning for past events!")
+    garbage = discord_events.events.garbage_collect(os.getcwd()) 
+    for i in garbage:
+        await remove(os.path.join(os.getcwd(),i))
+    print("removed "+str(len(garbage))+" items!")
+    await asyncio.sleep(30)
+
+
     
 bot.run(TOKEN)
--- discord_events.py
+++ discord_events.py
@@ -2,26 +2,55 @@ import os
 import json
 import random
 import string
+from datetime import datetime
+import glob
 
 class events:
     def list(PATH):
         event_list = {}
         for file in os.listdir(PATH):
             if file.endswith(".json"):
-                json_file = open(PATH,file)
+                json_file = open(os.path.join(PATH,file))
                 loader = json.load(json_file)
+                print(loader)
                 event_list[loader["uID"]]= { key:loader[key] for key in ["event_date","event_name","event_participants"]}
                 json_file.close()
         return event_list
     
-    def generate_event(PATH,event_date,event_name):
-        event_data = {"event_name":event_name,"event_date":event_data,"event_participants":[]}
-        uID = ""
+    def generate_event(PATH,event_name,event_date):
         while True:
-            uID = "".join(random.choice(string.ascii_letters) for i in range(8))
-            if os.path.isfile(PATH,uID+".json") is False:
+            uID = "".join(random.choice(string.ascii_letters) for i in range(8))+".json"
+            new_path = os.path.join(PATH,uID)
+            if os.path.isfile(PATH+"/"+uID+".json") is False:
                 break
-        f = open(PATH,uID+".json")
+        event_data = {"event_name":event_name,"event_date":event_date,"event_participants":[],"uID":uID}   
+        f = open(new_path,"w+")
         json.dump(event_data,f)
         f.close()
         return uID
+    
+    def garbage_collect(PATH):
+        curr_day,curr_month,curr_year = datetime.now().strftime('%d/%m/%Y').split("/")
+        cnt = 0
+        to_remove = []
+        os.chdir(PATH)
+        for file in glob.glob("*.json"):
+            print(file)
+            json_file = open(os.path.join(PATH,file))
+            loader = json.load(json_file)
+            event_time = str(loader["event_date"])
+            print(event_time)
+            day,month,year =event_time[0:event_time.find(":")].split("/")
+            print(f'{day},{month},{year}')
+            if int(curr_year)>int(year):
+                cnt+=1
+                to_remove.append(file)
+            elif int(curr_month)>int(month):
+                cnt+=1
+                to_remove.append(file)
+            elif int(curr_day)>int(day):
+                print("found outdated file")
+                cnt+=1
+                to_remove.append(file)
+        return to_remove
+                    
